cd924e9d188784abd6c8014f0614afed8a2c940d
Luke P <lukeponiatowski@gmail.com>
2017-01-23 21:01:29 -0500

    v 1.0.0
    Initial commit

---
+++ staticNodeWebserver.js
@@ -0,0 +1,34 @@
+// JavaScript Web Server
+
+// Required resources
+var express = require('express');
+
+// Init. vars
+var app = express();
+var root = __dirname;
+
+// Get index
+app.get('/', function (req, res) {  // Loads hompage on request
+    res.sendFile( __dirname + '/www/index.html', function (err) {
+        if (err) {
+           res.status(err.status).end();
+        }
+    });
+});
+
+// Get resources
+app.get('/*', function (req, res) {  // Loads anything in the servers public dir, on request
+    res.sendFile( __dirname + '/www' + req.path, function (err) {
+        if (err) {
+            res.status(err.status).end();
+        }
+    });
+});
+
+
+
+// Server Listen
+var port = process.env.PORT || 8080
+app.listen(port , function() {
+    console.log('Server running at http:localhost:' + port  );
+});
